7ccdcd7fc4f4df92e210be2fd51e9541e9630c19
KDM121 <117851257+KDM121@users.noreply.github.com>
2023-10-12 10:14:11 +0000

    project

---
+++ pages/Projects.js
@@ -0,0 +1,18 @@
+import { Inter } from "next/font/google";
+import { Link } from "react-scroll";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Kierans Website</title>
+        <meta name="description" content="Kieran's Website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/Favicon.ico" />
+      </Head>
+      <main>test</main>
+    </>
+  );
+}
